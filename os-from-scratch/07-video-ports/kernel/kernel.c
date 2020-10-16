3adbc6493d435707466e02db464a3fcb081a7440
Kostas Makedos <kostas.makedos@gmail.com>
2020-10-17 00:24:13 +0300

    Add video

--- os-from-scratch/07-video-ports/kernel/kernel.c
+++ os-from-scratch/07-video-ports/kernel/kernel.c
@@ -1,19 +1,20 @@
 #include "../drivers/ports.h"
+#include "../drivers/screen.h"
 
 void main() {
     /* Screen cursor position: ask VGA control register (0x3d4) for bytes
      * 14 = high byte of cursor and 15 = low byte of cursor. */
-    port_byte_out(0x3d4, 14); /* Requesting byte 14: high byte of cursor pos */
+//    port_byte_out(0x3d4, 14); /* Requesting byte 14: high byte of cursor pos */
     /* Data is returned in VGA data register (0x3d5) */
-    int position = port_byte_in(0x3d5);
-    position = position << 8; /* high byte */
+//   int position = port_byte_in(0x3d5);
+//    position = position << 8; /* high byte */
 
-    port_byte_out(0x3d4, 15); /* requesting low byte */
-    position += port_byte_in(0x3d5);
+//    port_byte_out(0x3d4, 15); /* requesting low byte */
+//    position += port_byte_in(0x3d5);
 
     /* VGA 'cells' consist of the character and its control data
      * e.g. 'white on black background', 'red text on white bg', etc */
-    int offset_from_vga = position * 2;
+ //   int offset_from_vga = position * 2;
 
     /* Now you can examine both variables using gdb, since we still
      * don't know how to print strings on screen. Run 'make debug' and
@@ -27,6 +28,12 @@ void main() {
     /* Let's write on the current cursor position, we already know how
      * to do that */
     char *vga = 0xb8000;
-    vga[offset_from_vga] = 'X';
-    vga[offset_from_vga+1] = 0x0f; /* White text on black background */
+    clear_screen();
+
+    kprint_at("X", 1, 6);
+    kprint_at("This text spans multiple lines", 75, 10);
+    kprint_at("There is a line\n break", 0, 20);
+    kprint("Another line\nbreak");
+    kprint_at("What happens if we run completely out of space?", 45,24);
+
 }
